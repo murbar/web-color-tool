d7b1a44af26bec31f77ce44a6602a804769a1e56
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-20 19:11:31 -0400

    Rough out values table

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Styles = styled.div`
+const Color = styled.div`
   width: 20rem;
   height: 20rem;
   background: ${p => p.rgbColor};
-  border-radius: 3rem;
+  border-radius: 1rem;
 `;
 
-export default function Preview({ rgb }) {
-  const rgbColor = `rgb(${rgb.R}, ${rgb.G}, ${rgb.B})`;
-  return <Styles rgbColor={rgbColor} />;
+const DisplayTable = styled.div`
+  & > div {
+    display: flex;
+    & > div {
+      width: 4rem;
+    }
+  }
+`;
+
+const ValuesTable = ({ colorValues }) => {
+  return (
+    <DisplayTable>
+      <div>
+        <div>RGB</div>
+        <div>{colorValues.rgb.R}</div>
+        <div>{colorValues.rgb.G}</div>
+        <div>{colorValues.rgb.B}</div>
+      </div>
+      <div>
+        <div>HSL</div>
+        <div>{colorValues.hsl.H}</div>
+        <div>{colorValues.hsl.S}%</div>
+        <div>{colorValues.hsl.L}%</div>
+      </div>
+      <div>
+        <div>Hex</div>
+        <div>{colorValues.hex}</div>
+        <div />
+        <div />
+      </div>
+    </DisplayTable>
+  );
+};
+
+export default function Preview({ colorValues }) {
+  const rgbColor = `rgb(${colorValues.rgb.R}, ${colorValues.rgb.G}, ${colorValues.rgb.B})`;
+  return (
+    <>
+      <Color rgbColor={rgbColor} />
+      <ValuesTable colorValues={colorValues} />
+    </>
+  );
 }
