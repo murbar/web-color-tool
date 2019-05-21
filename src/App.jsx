1c2bbeec32b019e2aa3dc67502e7e1efbc901da3
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-21 08:50:59 -0400

    Extract Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Preview from './components/Preview';
-import Input from './components/Input';
 import colorConvert from './colorConvert';
+import Inputs from './components/Inputs';
+import Button from './components/Button';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
-const Styles = styled.div``;
+const AppWrapper = styled.div``;
 
-const HexInputs = ({ handleChange, colorValues }) => {
-  // validate 3 or 6 chars, all 0-F
-  const [inputValid, setInputValid] = useState(true);
-  const [value, setValue] = useState('000000');
-
-  useEffect(() => {
-    setValue(colorValues.hex);
-  }, [colorValues]);
-
-  const re = /^([0-9a-f]{3}|[0-9a-f]{6})$/i;
-
-  const onChange = e => {
-    const v = e.target.value.replace(/#/, '');
-    setValue(v);
-    setInputValid(true);
-    const validValue = re.test(v);
-    if (!validValue) {
-      setInputValid(false);
-    } else {
-      handleChange(v);
-    }
-  };
-  return (
-    <>
-      Hex <Input type="text" placeholder="000000" value={value} onChange={onChange} maxLength="6" />
-      {!inputValid && <p>Invalid hex</p>}
-    </>
-  );
-};
+const Randomizer = styled(Button)`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+`;
 
 const deriveColorState = rgbValues => {
   return {
@@ -59,23 +38,16 @@ function App() {
     setColorValues(randomColorValues());
   };
 
-  // useEffect(() => {
-  //   setColorValues(calcValues([R, G, B]));
-  // }, [colorValues.rgb]);
-
-  const setHex = hexValue => {
-    const rgbValues = colorConvert.hex.toRgb(hexValue);
-    setColor(rgbValues);
-  };
-
   return (
-    <Styles>
-      <h1>Color Multi-tool</h1>
-      <button onClick={randomizeColor}>Randomize</button>
-      <Preview colorValues={colorValues} />
-      <h2>Inputs</h2>
-      <HexInputs handleChange={setHex} colorValues={colorValues} />
-    </Styles>
+    <Router>
+      <AppWrapper>
+        <h1>Color Multi-tool</h1>
+        <Randomizer onClick={randomizeColor}>Randomize</Randomizer>
+        <Preview colorValues={colorValues} />
+        <h2>Inputs</h2>
+        <Inputs setColor={setColor} colorValues={colorValues} />
+      </AppWrapper>
+    </Router>
   );
 }
 
