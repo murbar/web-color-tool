db766f785ba44f9f5e725f7804ea058ed35743e6
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-21 10:44:05 -0400

    Add color transition

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import colorConvert from '../colorConvert';
 import ColorValues from './ColorValues';
+import { useSpring, animated } from 'react-spring';
 
-const Color = styled.div`
+const ColorDisplay = styled(animated.div)`
   height: 45vh;
-  background: ${p => p.rgbCSS};
-  box-shadow: 0px 0 2rem 0px ${p => p.rgbCSS};
-`;
+  /* background: ${p => p.rgbCSS}; */
+  `;
 
 export default function Preview({ colorValues }) {
   const rgbCSS = colorConvert.rgb.toCSS(colorValues.rgb);
+  const color = useSpring({
+    background: rgbCSS,
+    boxShadow: `0px 0 2rem 0px ${rgbCSS}`
+  });
   return (
-    <>
-      <Color rgbCSS={rgbCSS}>
-        <ColorValues values={colorValues} />
-      </Color>
-    </>
+    <ColorDisplay style={color}>
+      <ColorValues values={colorValues} />
+    </ColorDisplay>
   );
 }
