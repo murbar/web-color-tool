39932a918f090994ca451fc5d08b45c6a9d376f8
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-21 08:51:06 -0400

    Add styles

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  background: hsla(0, 0%, 100%, 0.85);
+  color: #333;
+  font-family: 'Source Code Pro', monospace;
+  border: 0;
+  font-size: 1em;
+  padding: 0.5em 0.85em;
+  border-radius: 0.5em;
+  &:hover {
+    background: white;
+    cursor: pointer;
+  }
+`;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export default styled.input`
-  color: red;
+  background: #333;
+  color: #efefef;
+  font-family: 'Source Code Pro', monospace;
+  border: 0;
+  font-size: 2rem;
+  padding: 0.25em 0.5em;
+  text-align: right;
+  border-radius: 0.5em;
 `;
