9dc9f0627eed82a07496353d5e9957a03ccf946b
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-22 09:46:27 -0400

    Add inputs for ranges 0..100 & 0..360

---
+++ src/components/common/DegreeInput.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Input from './Input';
+
+const Styles = styled(Input)`
+  width: calc(3ch + 1em);
+`;
+
+// 0..360 with leading zeros
+const degreeMatch = /^(0?[0-9]?[0-9]|[1-2][0-9][0-9]|3[0-5][0-9]|360)$/;
+
+const DegreeInput = props => {
+  const { name, value, onChange } = props;
+
+  const handleChange = e => {
+    const { value } = e.target;
+    if (degreeMatch.test(value) || value === '') {
+      onChange(e, parseInt(value) || 0);
+    }
+  };
+
+  return (
+    <Styles
+      {...props}
+      name={name}
+      value={value}
+      onChange={handleChange}
+      type="text"
+      placeholder="360"
+      maxLength="3"
+    />
+  );
+};
+
+DegreeInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default DegreeInput;
---
+++ src/components/common/HectoInput.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Input from './Input';
+
+const Styles = styled(Input)`
+  width: calc(6ch + 1em);
+`;
+
+// 0..100 with leading zeros
+const hectoMatch = /^0*(?:[0-9][0-9]?|100)$/;
+
+const HectoInput = props => {
+  const { name, value, onChange } = props;
+
+  const handleChange = e => {
+    const { value } = e.target;
+    if (hectoMatch.test(value) || value === '') {
+      onChange(e, parseInt(value) || 0);
+    }
+  };
+
+  return (
+    <Styles
+      {...props}
+      name={name}
+      value={value}
+      onChange={handleChange}
+      type="text"
+      placeholder="100"
+      maxLength="3"
+    />
+  );
+};
+
+HectoInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default HectoInput;
