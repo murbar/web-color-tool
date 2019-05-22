df5e072aeaaca7bed2053a49e20e06fe5b1ac610
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-22 13:36:55 -0400

    Add basic layout for inputs

--- src/components/ValueInputs.jsx
+++ src/components/ValueInputs.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import HexForm from './HexForm';
 import RgbForm from './RgbForm';
 import HslForm from './HslForm';
 
+const Styles = styled.div`
+  & > div {
+    margin-bottom: 1.5rem;
+  }
+  label {
+    margin-right: 1rem;
+  }
+  input {
+    margin-right: 1rem;
+  }
+`;
+
 const Inputs = ({ setColor, colorValues }) => {
   return (
-    <div>
+    <Styles>
       <HexForm setColor={setColor} colorValues={colorValues} />
       <RgbForm setColor={setColor} colorValues={colorValues} />
       <HslForm setColor={setColor} colorValues={colorValues} />
-    </div>
+    </Styles>
   );
 };
 
