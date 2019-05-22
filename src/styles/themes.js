62dd9c1fbab49838409302486e687aa8f189e116
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-22 19:10:41 -0400

    Add inputColor

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -2,14 +2,16 @@ const dark = {
   backgroundColor: '#111111',
   textColor: '#efefef',
   overlayColor: 'rgba(255, 255, 255, 0.5)',
-  highlightColor: 'hsl(168, 81%, 56%)'
+  highlightColor: 'hsl(168, 81%, 56%)',
+  inputColor: '#333333'
 };
 
 const light = {
   backgroundColor: '#efefef',
   textColor: '#111111',
   overlayColor: 'rgba(0, 0, 0, 0.5)',
-  highlightColor: 'hsl(168, 81%, 56%)'
+  highlightColor: 'hsl(168, 81%, 56%)',
+  inputColor: '#CCCCCC'
 };
 
 export { light, dark };
