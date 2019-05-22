941e4554ace384976657e7c1e65570cf5c05d85e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-21 23:31:12 -0400

    Add overlay color

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,11 +1,13 @@
 const light = {
   backgroundColor: '#111111',
-  textColor: '#efefef'
+  textColor: '#efefef',
+  overlayColor: 'rgba(255, 255, 255, 0.7)'
 };
 
 const dark = {
   backgroundColor: '#efefef',
-  textColor: '#111111'
+  textColor: '#111111',
+  overlayColor: 'rgba(0, 0, 0, 0.7)'
 };
 
 export { light, dark };
