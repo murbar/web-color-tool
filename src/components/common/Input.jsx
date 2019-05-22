5a2fd0b1c6de22dbd8b46e2c18d72d4944100380
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-22 11:50:55 -0400

    Style error state

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,4 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const errorState = css`
+  box-shadow: 0 0 0 0.1em inset rgba(200, 20, 60, 0.8);
+`;
 
 export default styled.input`
   background: #333;
@@ -9,4 +13,5 @@ export default styled.input`
   padding: 0.25em 0.5em;
   text-align: right;
   border-radius: 0.5em;
+  ${p => p.error && errorState}
 `;
