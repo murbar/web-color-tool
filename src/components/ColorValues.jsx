ce11fd987ba2512d0f0ef2b5ce45cf5a362d5bb0
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-06-02 16:07:29 -0400

    Copy CSS value on click

--- src/components/ColorValues.jsx
+++ src/components/ColorValues.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import colorConvert from '../colorConvert';
+import colorConvert from 'colorConvert';
 import { animated, useSpring } from 'react-spring';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Styles = styled.div`
   display: flex;
   flex-direction: column;
-  & > div {
+  .values {
     margin: 1rem 1rem 0 auto;
     padding: 0.5em;
     line-height: 1;
@@ -17,6 +18,20 @@ const Styles = styled.div`
   }
 `;
 
+const CopyOnClick = ({ string, children }) => {
+  return (
+    <CopyToClipboard text={string} onCopy={() => console.log(`copied ${string}!`)}>
+      <div
+        className="values"
+        title={`Copy CSS to clipboard: "${string}"`}
+        style={{ cursor: 'pointer' }}
+      >
+        {children}
+      </div>
+    </CopyToClipboard>
+  );
+};
+
 export default function ColorValues({ colorValues }) {
   const values = useSpring({
     config: { duration: 400 },
@@ -24,9 +39,15 @@ export default function ColorValues({ colorValues }) {
     hsl: colorValues.hsl
   });
 
+  const cssStrings = {
+    rgb: `rgb (${colorValues.rgb[0]}, ${colorValues.rgb[1]}, ${colorValues.rgb[2]})`,
+    hsl: `hsl (${colorValues.hsl[0]}, ${colorValues.hsl[1]}%, ${colorValues.hsl[2]}%)`,
+    hex: `#${colorValues.hex}`
+  };
+
   return (
     <Styles>
-      <div>
+      <CopyOnClick string={cssStrings.rgb}>
         RGB
         <animated.span>
           {values.rgb.interpolate((...rgb) => {
@@ -34,8 +55,8 @@ export default function ColorValues({ colorValues }) {
             return ` ${r}, ${g}, ${b}`;
           })}
         </animated.span>
-      </div>
-      <div>
+      </CopyOnClick>
+      <CopyOnClick string={cssStrings.hsl}>
         HSL
         <animated.span>
           {values.hsl.interpolate((...hsl) => {
@@ -43,13 +64,13 @@ export default function ColorValues({ colorValues }) {
             return ` ${h}, ${s}%, ${l}%`;
           })}
         </animated.span>
-      </div>
-      <div>
+      </CopyOnClick>
+      <CopyOnClick string={cssStrings.hex}>
         HEX #
         <animated.span>
           {values.rgb.interpolate((r, g, b) => colorConvert.rgb.toHex([r, g, b]))}
         </animated.span>
-      </div>
+      </CopyOnClick>
     </Styles>
   );
 }
