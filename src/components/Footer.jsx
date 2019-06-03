d5d0fce66a9a2c9d04384747816040fbcf30a6f0
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-06-02 20:09:28 -0400

    Replace emoji with svg icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Heart } from 'icons/heart.svg';
 
 const Styles = styled.div`
   padding: 2rem 0;
   text-align: center;
+  svg {
+    height: 1em;
+    color: crimson;
+    margin: 0 -0.25rem;
+  }
 `;
 
 const Footer = () => {
@@ -11,7 +17,7 @@ const Footer = () => {
     <Styles>
       Made with{' '}
       <span role="img" aria-label="love">
-        ❤️
+        <Heart />
       </span>{' '}
       and React by{' '}
       <a href="https://joelb.dev" title="Joel Bartlett's portfolio">
