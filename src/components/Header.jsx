9d55f2c8b5ed730293bd26807bfa53ffc28339af
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-06-28 09:21:41 -0400

    Style header, update copy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  h1 span {
+    display: block;
+    font-size: 0.8em;
+    font-weight: normal;
+    line-height: 1;
+  }
+`;
 
 const Header = () => {
-  return <h1>Color Converter RGB/HSL/HEX</h1>;
+  return (
+    <StyledDiv>
+      <h1>
+        Color Converter <span>RGB &rarr; HSL &rarr; HEX</span>{' '}
+      </h1>
+    </StyledDiv>
+  );
 };
 
 export default Header;
