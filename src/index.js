c852db49c830ecbcb3284a1f1ad92428d9557d15
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-09 15:49:44 -0400

    Add router switch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import App from './App';
 import { validHsl, validRgb, validHex } from 'helpers';
 import colorConvert from 'colorConvert';
@@ -9,36 +9,39 @@ const Root = () => {
   return (
     // <React.StrictMode>
     <Router>
-      <Route path="/" exact render={() => <App initialColor={null} />} />
-      <Route
-        path="/hsl/:h/:s/:l"
-        render={({ match }) => {
-          const { h, s, l } = match.params;
-          return validHsl(h, s, l) ? (
-            <App initialColor={colorConvert.hsl.toRgb([h, s, l])} />
-          ) : (
-            <Redirect to="/" />
-          );
-        }}
-      />
-      <Route
-        path="/rgb/:r/:g/:b"
-        render={({ match }) => {
-          const { r, g, b } = match.params;
-          return validRgb(r, g, b) ? <App initialColor={[r, g, b]} /> : <Redirect to="/" />;
-        }}
-      />
-      <Route
-        path="/hex/:hex"
-        render={({ match }) => {
-          const { hex } = match.params;
-          return validHex(hex) ? (
-            <App initialColor={colorConvert.hex.toRgb(hex)} />
-          ) : (
-            <Redirect to="/" />
-          );
-        }}
-      />
+      <Switch>
+        <Route path="/" exact render={() => <App initialColor={null} />} />
+        <Route
+          path="/hsl/:h/:s/:l"
+          render={({ match }) => {
+            const { h, s, l } = match.params;
+            return validHsl(h, s, l) ? (
+              <App initialColor={colorConvert.hsl.toRgb([h, s, l])} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
+        />
+        <Route
+          path="/rgb/:r/:g/:b"
+          render={({ match }) => {
+            const { r, g, b } = match.params;
+            return validRgb(r, g, b) ? <App initialColor={[r, g, b]} /> : <Redirect to="/" />;
+          }}
+        />
+        <Route
+          path="/hex/:hex"
+          render={({ match }) => {
+            const { hex } = match.params;
+            return validHex(hex) ? (
+              <App initialColor={colorConvert.hex.toRgb(hex)} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
+        />
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
     </Router>
     // </React.StrictMode>
   );
