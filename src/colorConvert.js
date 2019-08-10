255b7641d795d451047140d93dcd022e9a0a6689
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-10 11:57:46 -0400

    Add precision HSL functions

--- src/colorConvert.js
+++ src/colorConvert.js
@@ -1,4 +1,7 @@
 import { trueMod } from 'helpers';
+import { hslScaleFactor } from 'config';
+
+const HSL_SCALE = hslScaleFactor;
 
 // 'fff'/'FFFFFF' -> ['FF', 'FF', 'FF']
 const splitHex = hexValue => {
@@ -23,23 +26,23 @@ const rgbToHex = rgbValues => {
     .toUpperCase();
 };
 
-// [255, 255, 255] -> [0, 0, 100]
+// [255, 255, 255] -> [1440, 400, 400]
 // https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB
 // https://stackoverflow.com/questions/39118528/rgb-to-hsl-conversion
 // https://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
-const rgbToHsl = rgbValues => {
+const rgbToHsl4x = rgbValues => {
   // convert to value between 0 and 1
   const rgb = rgbValues.map(n => parseInt(n) / 255);
   const [min, max] = [Math.min(...rgb), Math.max(...rgb)];
   const L = (min + max) / 2;
-  const lum = Math.round(L * 100);
+  const lum = Math.round(L * 100 * HSL_SCALE);
 
   // no sat, so no hue, we have a shade of gray
   if (max === min) return [0, 0, lum];
 
   const chroma = max - min;
   const S = chroma / (1 - Math.abs(2 * L - 1));
-  const sat = Math.round(S * 100);
+  const sat = Math.round(S * 100 * HSL_SCALE);
 
   const [R, G, B] = rgb;
   const hueMaxChannel = {
@@ -48,9 +51,14 @@ const rgbToHsl = rgbValues => {
     '2': (R - G) / chroma + 4
   };
   const H = hueMaxChannel[rgb.indexOf(max)];
-  const hue = Math.round(H * 60);
-
+  const hue = Math.round(H * 60 * HSL_SCALE);
   return [hue, sat, lum];
+};
+
+// [255, 255, 255] -> [360, 100, 100]
+const rgbToHsl = rgbValues => {
+  const hsl4x = rgbToHsl4x(rgbValues);
+  return hsl4x.map(v => Math.round(v / 4));
 };
 
 // 'FFF'/'FFFFFF' -> [255, 255, 255]
@@ -58,16 +66,22 @@ const hexToRgb = hexValue => {
   return splitHex(hexValue).map(h => parseInt(h, 16));
 };
 
-// 'FFF'/'FFFFFF' -> [0, 0, 100]
+// 'FFF'/'FFFFFF' -> [360, 100, 100]
 const hexToHsl = hexValue => {
   const rgbValues = hexToRgb(hexValue);
   return rgbToHsl(rgbValues);
 };
 
-// [0, 0, 100] -> '255, 255, 255'
+// 'FFF'/'FFFFFF' -> [1440, 400, 400]
+const hexToHsl4x = hexValue => {
+  const rgbValues = hexToRgb(hexValue);
+  return rgbToHsl4x(rgbValues);
+};
+
+// [1440, 400, 400] -> '255, 255, 255'
 // https://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
-const hslToRgb = hslValues => {
-  let [H, S, L] = hslValues.map(v => parseInt(v));
+const hsl4xToRgb = hslValues4x => {
+  let [H, S, L] = hslValues4x.map(v => parseInt(v) / HSL_SCALE);
 
   const sat = S / 100;
   const lum = L / 100;
@@ -94,19 +108,34 @@ const hslToRgb = hslValues => {
     if (3 * val < 2) return y + (x - y) * (2 / 3 - val) * 6;
     return y;
   };
-  return protoRBG.map(calcValue).map(v => trueMod(Math.round(255 * v), 255));
-  // return protoRBG.map(calcValue).map(v => Math.round(255 * v));
+  // beware modulo and off-by-1s! 256 here, not 255
+  return protoRBG.map(calcValue).map(v => trueMod(Math.round(255 * v), 256));
 };
 
-// [0, 0, 100] -> 'ffffff'
+const hslToRgb = hslValues => {
+  return hsl4xToRgb(hslValues.map(v => v * 4));
+};
+
+// [360, 100, 100] -> 'ffffff'
 const hslToHex = hslValues => {
   const rgbValues = hslToRgb(hslValues.map(v => parseInt(v)));
+  return rgbToHex(rgbValues);
+};
+
+// [1440, 400, 400] -> 'ffffff'
+const hsl4xToHex = hslValues4x => {
+  const rgbValues = hsl4xToRgb(hslValues4x);
   return rgbToHex(rgbValues);
 };
 
 const hslValuesToCSS = hslValues => {
   const [H, S, L] = hslValues;
   return `hsl(${H}, ${S}%, ${L}%)`;
+};
+
+const hslValues4xToCSS = hslValues4x => {
+  const [H, S, L] = hslValues4x;
+  return `hsl(${H / 4}, ${S / 4}%, ${L / 4}%)`;
 };
 
 const rgbValuesToCSS = rgbValues => {
@@ -119,19 +148,26 @@ const hexValuesToCSS = hexValue => {
 };
 
 export default {
-  rgb: {
-    toHex: rgbToHex,
-    toHsl: rgbToHsl,
-    toCSS: rgbValuesToCSS
+  hsl4x: {
+    toRgb: hsl4xToRgb,
+    toHex: hsl4xToHex,
+    toCSS: hslValues4xToCSS
   },
   hsl: {
     toRgb: hslToRgb,
     toHex: hslToHex,
     toCSS: hslValuesToCSS
   },
+  rgb: {
+    toHsl: rgbToHsl,
+    toHsl4x: rgbToHsl4x,
+    toHex: rgbToHex,
+    toCSS: rgbValuesToCSS
+  },
   hex: {
-    toRgb: hexToRgb,
     toHsl: hexToHsl,
+    toHsl4x: hexToHsl4x,
+    toRgb: hexToRgb,
     toCSS: hexValuesToCSS,
     normalize: hex => splitHex(hex).join('')
   }
