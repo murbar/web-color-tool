ba38528d062dcd30804e661651599e44f96c40b2
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-10 21:42:10 -0400

    Rename var

--- src/config.js
+++ src/config.js
@@ -1,24 +1,31 @@
 const version = '2019.8.10';
 
+const initPreferences = {
+  darkTheme: true
+};
+
+const harmonyConstants = {
+  CO: 'Complementary',
+  MO: 'Monochromatic',
+  AN: 'Analogous',
+  SP: 'Split Complementary',
+  TR: 'Triadic',
+  TE: 'Tetradic'
+};
+
 export default {
   version,
-  localStorageStrings: {
+  publicURL: 'https://color.joelb.dev',
+  pageTitle: 'Web color tool for developers | Convert RGB/HSL/Hex & explore harmonies',
+  localStorageKeys: {
     color: `color-joeb-dev-color-${version}`,
     theme: `color-joeb-dev-theme-${version}`,
     preferences: `color-joeb-dev-prefs-${version}`
   },
-  publicURL: 'https://color.joelb.dev',
-  pageTitle: 'Web color tool for developers | Convert RGB/HSL/Hex & explore harmonies',
   GAPropertyId: 'UA-140727716-1',
+  initPreferences,
   env: process.env.NODE_ENV,
   transitionDurationMs: 400,
   hslScaleFactor: 4,
-  harmonyConstants: {
-    CO: 'Complementary',
-    MO: 'Monochromatic',
-    AN: 'Analogous',
-    SP: 'Split Complementary',
-    TR: 'Triadic',
-    TE: 'Tetradic'
-  }
+  harmonyConstants
 };
